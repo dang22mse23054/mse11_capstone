2b141f4b4903ec781734819fdefa9738d0de510b
Cuong Nguyen <yntcuong2103@gmail.com>
2023-12-05 14:30:11 +0000

    roll back valid sampling

--- source_code/data/model.py
+++ source_code/data/model.py
@@ -36,8 +36,6 @@ class FaceDetectionModel(LightningModule):
 		return self.model(x)
 	
 	def training_step(self, batch, batch_idx):
-		print(f'-----------training_step------ {batch_idx}')
-
 		images, targets = batch
 		targets = [{k: v for k, v in t.items()} for t in targets]
 		loss_dict = self.model(images, targets)
@@ -78,37 +76,23 @@ class FaceDetectionModel(LightningModule):
 		)
 
 		# total loss
-		print('-----------training_step  _0------')
-
 		losses = sum(loss for loss in loss_dict.values())
-		print('-----------training_step  _1------')
-
-
 		self.log('train_loss', losses, prog_bar=True, on_step=True, on_epoch=True)
-
-		# debug: skip calculating loss
-		losses = torch.tensor(0.)
-		losses.requires_grad = True
-		print(f'-----------training_step  _loss ------ {losses}')
 
 		return losses
 
 	def eval_step(self, batch, batch_idx, prefix: str):
-		print('-----------eval_step------')
-
 		import random
-		# if random.random() < 0.1:
-		images, targets = batch
-		preds = self.model(images)
-		selected = random.sample(range(len(images)), len(images) // 5)
-		self.map.update([preds[i] for i in selected], [targets[i] for i in selected])
+		if random.random() < 0.1:
+			images, targets = batch
+			preds = self.model(images)
+			selected = random.sample(range(len(images)), len(images) // 5)
+			self.map.update([preds[i] for i in selected], [targets[i] for i in selected])
 
 	def validation_step(self, batch, batch_idx):
 		return self.eval_step(batch, batch_idx, "val")
 	
 	def configure_optimizers(self):
-		print('-----------configure_optimizers------')
-
 		optimizer = optim.AdamW(self.parameters(), lr=self.hparams.lr, weight_decay=self.hparams.weight_decay)
 		# We will reduce the learning rate by 0.1 after 20 epochs
 		scheduler = lr_scheduler.LambdaLR(optimizer, lambda epoch: 0.1 ** (epoch // 20))
