195d5e97c1b78672d039af8a872647f2754577fa
Ha Hai Dang <ha_hai_dang@ca-adv.co.jp>
2023-12-10 21:02:53 +0700

    Change trainer file

--- source_code/data/trainer.py
+++ source_code/data/trainer.py
@@ -16,22 +16,44 @@ if __name__ == "__main__":
 	# model.model.backbone.load_state_dict(OrderedDict({k.replace('model.backbone.', ''):v for k, v in checkpoint['state_dict'].items() 
 	#                                                   if 'model.backbone.' in k}))
 
-	data = FaceDataLoader(batch_size=10, workers=5)
+	data = FaceDataLoader(batch_size=48, workers=5)
+
+
+	# Save top3 models wrt precision
+	# on_best_precision = ModelCheckpoint(
+	# 	filepath=filepath + "{epoch}-{precision}",
+	# 	monitor="precision",
+	# 	save_top_k=3,
+	# 	mode="max",
+	# )
+	# # Save top3 models wrt recall
+	# on_best_recall = ModelCheckpoint(
+	# 	filepath=filepath + "{epoch}-{recall}",
+	# 	monitor="recall",
+	# 	save_top_k=3,
+	# 	mode="max",
+	# )
+	# # Save the model every 5 epochs
+	# every_five_epochs = ModelCheckpoint(
+	# 	period=5,
+	# 	save_top_k=-1,
+	# 	save_last=True,
+	# )
 
 	trainer = Trainer(
 		accelerator="cpu",
 		# accelerator="mps",
 		# checkpoint_callback=True,
-		# callbacks = [
-		#     LearningRateMonitor(logging_interval='step'),
-		#     ModelCheckpoint(filename='{epoch}-{val_map:.4f}', save_top_k=5, monitor='val_map', mode='max'),
-		# ], 
+		callbacks = [
+		    LearningRateMonitor(logging_interval='step'),
+		    # ModelCheckpoint(dirpath='', filename='{epoch}-{val_acc:.4f}', save_top_k=5, monitor='val_acc', mode='max'),
+		], 
 		# check_val_every_n_epoch=1,
-		# fast_dev_run=False,
-		# default_root_dir='checkpoint',
+		# fast_dev_run=True,
+		default_root_dir='checkpoint',
 
 		# deterministic=False, 
-		max_epochs=1, 
+		max_epochs=10, 
 		# log_every_n_steps=2,
 
 		# gpus = [2],
