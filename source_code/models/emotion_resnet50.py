3c35d86e4df23ee0bccdbe41480c3ac8e70eb2bd
Ha Hai Dang <ha_hai_dang@ca-adv.co.jp>
2024-01-23 21:28:02 +0700

    Upddate code

--- source_code/models/emotion_resnet50.py
+++ source_code/models/emotion_resnet50.py
@@ -5,7 +5,7 @@ from common.constants import Constants
 EMOTION = Constants.Emotion()
 NUM_OF_EMOTIONS = len(EMOTION.Groups)
 
-class EmotionResNet50(nn.Module):
+class EmotionResNet50v1(nn.Module):
 	def __init__(self,
 				emotion_classes: int = NUM_OF_EMOTIONS,
 				output_channels: int = 24,
@@ -69,6 +69,47 @@ class EmotionResNet50(nn.Module):
 		x = self.fc3(x)
 		x = self.batch_norm4(x)
 		
+		x = self.relu(x)
+		x = self.output(x)
+
+		# khi dùng CrossEntropyLoss() thì ko cần dùng softmax ở trong model nữa
+		# x = self.softmax(x)
+		return x
+
+class EmotionResNet50v2(nn.Module):
+	def __init__(self,
+				emotion_classes: int = NUM_OF_EMOTIONS,
+				output_channels: int = 24,
+	):
+		super().__init__()
+		
+		# Dùng model resnet50 đã train trước đó, 
+		# nhg loại bỏ 1 OR 2 layer cuối cùng (avgpool và fc)
+		self.base_model = models.resnet50(pretrained=True)
+
+		# ở đây là loại bỏ layer cuối cùng
+		# giống như self.encoder = nn.Sequential(*list(model.children())[:-1]) của age_gender_resnet50
+		# lớp Identity này không làm gì cả, chỉ lấy input và trả về chính nó
+		self.base_model.fc = nn.Identity()
+
+		# bắt đầu từ đây, nối tiếp nhiều layer mới
+		self.flatten = nn.Flatten()
+		self.fc1 = nn.Linear(2048, output_channels)
+		self.dropout = nn.Dropout(0.6)
+		self.fc2 = nn.Linear(output_channels, output_channels)
+		self.relu = nn.ReLU()
+
+		# khi dùng CrossEntropyLoss() thì ko cần dùng softmax ở trong model nữa
+		# self.softmax = nn.Softmax(dim=1)
+	
+	def forward(self, x):
+		# Feature extraction using ResNet50
+		x = self.base_model(x)
+		x = self.flatten(x)
+		x = self.fc1(x)
+		x = self.relu(x)
+		x = self.dropout(x)
+		x = self.fc2(x)
 		x = self.relu(x)
 		x = self.output(x)
 
